45ab9d684b311be48d642b05ff2e123fa757cbee
Busayo O <omolorun@users.noreply.github.com>
2021-07-01 10:32:58 -0700

    updated app

---
+++ WorldFlags/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  WorldFlags
+//
+//  Created by Oluwabusayo Olorunnipa on 7/1/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage : String?
+    var countryName : String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = countryName
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(buttonTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func buttonTapped(){
+        guard let image = imageView.image?.pngData()
+        else {
+            print("No image found")
+            return
+        }
+        
+        let ac = UIActivityViewController(activityItems: ["Check out the flag for \(countryName!)", image], applicationActivities:[])
+        
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    
+    }
+    
+
+
+}
--- WorldFlags/ViewController.swift
+++ WorldFlags/ViewController.swift
@@ -4,16 +4,77 @@
 //
 //  Created by Oluwabusayo Olorunnipa on 6/30/21.
 //
+// How to make this app better:
+// Include all the world flags from the images directory
+// Group countries by continents or regions
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var flags = [String]()
+    var names = ["af.png":"Afghanistan",
+                    "aq.png":"Antarctica",
+                    "ag.png":"Antigua and Barbuda",
+                    "ae.png":"United Arab Emirates",
+                    "ar.png":"Argentina",
+                    "as.png":"American Samoa",
+                    "ad.png":"Andorra",
+                    "aw.png":"Aruba",
+                    "at.png":"Austria",
+                    "au.png":"Australia",
+                    "ax.png":"Ãland Islands",
+                    "ao.png":"Angola",
+                    "al.png":"Albania",
+                    "am.png":"Armenia",
+                    "az.png":"Azerbaijan",
+                    "ai.png":"Anguilla"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "World Flags"
+        navigationController?.navigationBar.prefersLargeTitles = true
+            
+                
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+                
+        // Select only countries whose code begin with a
+        for item in items{
+            if item.hasPrefix("a"){
+                flags.append(item)
+            }
+            flags.sort()
+        }
+        // print(flags)
+        //print(flags.count)
+        
+        
     }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return flags.count
+       }
+       
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row].prefix(2).uppercased()
+        cell.imageView?.image = UIImage(named: flags[indexPath.row])
+        
+        return cell
 
-
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            
+            vc.selectedImage = flags[indexPath.row]
+            vc.countryName = names[flags[indexPath.row]] ?? "Unknown"
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    
+    }
 }
 
